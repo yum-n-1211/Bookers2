55f61cadbe43e8f26a6cca591acd4c3937ad7178
yum-n-1211 <92237680+yum-n-1211@users.noreply.github.com>
2022-01-26 14:05:44 +0000

    [Add]books_controller_new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about' =>"homes#about" ,as: 'about'
+  
+  resources :books, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
