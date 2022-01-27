a48c33ffcd9d3956cc306ac63f39d1e9b6b8a8dc
yum-n-1211 <92237680+yum-n-1211@users.noreply.github.com>
2022-01-27 08:37:22 +0000

    [Finish]ユーザー情報の編集機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' =>"homes#about" ,as: 'about'
   
   resources :books
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
