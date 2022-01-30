f5247bd433d4870c98a70dedff4e15ad158357b0
yum-n-1211 <92237680+yum-n-1211@users.noreply.github.com>
2022-01-30 05:19:32 +0000

    [Finish]フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,18 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
-    
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      render :index
+    end
   end
 
   def index
@@ -26,9 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
-  root to: "homes#top"
+  root to: "homes#top" ,as: 'top'
   get 'homes/about' =>"homes#about" ,as: 'about'
 
   resources :books, only: [:index, :show, :edit, :update, :create, :destroy]
